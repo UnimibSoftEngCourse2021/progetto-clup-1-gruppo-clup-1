7dee3f25db952aef99729409e19bd2a3dbd5f495
tommasozanelli <t.zanelli@campus.unimib.it>
2021-01-07 19:47:48 +0100

    Added Booking class and BookingDao class

---
+++ engSw2020.clup/src/main/webapp/DAO/BookingDao.java
@@ -0,0 +1,43 @@
+package main.webapp.DAO;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.time.*;
+import java.util.ArrayList;
+
+import main.webapp.model.Booking;
+import main.webapp.util.*;
+
+public class BookingDao {
+
+	public ArrayList<Booking> getBooking(){
+	  ArrayList<Booking> bookingList = new ArrayList<Booking>();
+	  Connection con = null;
+      Statement statement = null;
+      ResultSet resultSet = null;
+      
+      try{        	 
+
+          con = DBConnection.createConnection(); 
+          statement = con.createStatement(); 
+          resultSet = statement.executeQuery("select * from Booking");
+          while(resultSet.next()) {
+        	  Booking bookingBean = new Booking();
+        	  bookingBean.setIdBooking(resultSet.getInt("idBooking"));
+        	  bookingBean.setNumber(resultSet.getInt("Number"));
+        	  bookingBean.setBookingDate(resultSet.getDate("bookingDate"));
+        	  bookingBean.setArrivalTime(resultSet.getTime("ArrivalTime"));
+        	  bookingBean.setFinishTime(resultSet.getTime("FinishTime"));
+        	  bookingList.add(bookingBean);
+          }
+          return bookingList;
+       }
+      		
+      catch(Exception e)
+      {
+    	  e.printStackTrace();
+      }
+	return bookingList;
+	}
+}
+
---
+++ engSw2020.clup/src/main/webapp/model/Booking.java
@@ -0,0 +1,44 @@
+package main.webapp.model;
+
+import java.sql.Date;
+import java.sql.Time;
+
+public class Booking {
+	int idBooking;
+	int number;
+	Date bookingDate;
+	Time arrivalTime;
+	Time finishTime;
+	
+	public int getIdBooking() {
+		return idBooking;
+	}
+	public void setIdBooking(int idBooking) {
+		this.idBooking = idBooking;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public Time getArrivalTime() {
+		return arrivalTime;
+	}
+	public void setArrivalTime(Time arrivalTime) {
+		this.arrivalTime = arrivalTime;
+	}
+	public Time getFinishTime() {
+		return finishTime;
+	}
+	public void setFinishTime(Time finishTime) {
+		this.finishTime = finishTime;
+	}
+	public Date getBookingDate() {
+		return bookingDate;
+	}
+	public void setBookingDate(Date bookingDate) {
+		this.bookingDate=bookingDate;
+	}
+	
+}
