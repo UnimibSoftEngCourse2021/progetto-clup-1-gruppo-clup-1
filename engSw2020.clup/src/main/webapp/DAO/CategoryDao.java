0a6a42fd3183866ae03161847783a973ecd73520
Nasic-Elvis <e.nasic@campus.unimib.it>
2021-01-22 19:37:24 +0100

    Updated bugs CategoryDao and StoreDao

--- engSw2020.clup/src/main/webapp/DAO/CategoryDao.java
+++ engSw2020.clup/src/main/webapp/DAO/CategoryDao.java
@@ -1,188 +1,136 @@
 package main.webapp.DAO;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.sql.Time;
 import java.util.ArrayList;
-import main.webapp.model.Category;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-import main.webapp.model.Booking;
+import main.webapp.model.Category;
 import main.webapp.model.User;
 import main.webapp.util.DBConnection;
 
 public class CategoryDao {
-	
-	
-	public ArrayList<Category> getCategory(int idStoreUser) throws SQLException{
-		  ArrayList<Category> categoryList = new ArrayList<Category>();
-		  Connection con = null;
-	      Statement statement = null;
-	      ResultSet resultSet = null;
-	      
-	      try{        	 
 
-	          con = DBConnection.createConnection(); 
-	          statement = con.createStatement();
-	          String query = "SELECT category.idCategory, Name, Description FROM category INNER JOIN categoryinstore ON category.idCategory = categoryinstore.idCategory WHERE idStore = ?";
-	          		
-	          
-	          	PreparedStatement preparedStatement  = con.prepareStatement(query);
-	      		preparedStatement.setInt(1, idStoreUser);
+	private final static Logger log = Logger.getLogger(LoginDao.class.getName());
 
-	      		resultSet = preparedStatement.executeQuery(); 
-	          while(resultSet.next()) {
-	        	  Category categoryBean = new Category();
-	        	  User user = new User();
-	        	  categoryBean.setIdCategory(resultSet.getInt("idCategory"));
-	        	  categoryBean.setName(resultSet.getString("Name"));
-	        	  categoryBean.setDescription(resultSet.getString("Description"));
-	        	  categoryBean.setIdStore(idStoreUser);
+	public ArrayList<Category> getCategory(int idStoreUser) throws SQLException {
 
-	        	  categoryList.add(categoryBean);
-	          }
-	          return categoryList;
-	       }
-	      		
-	      catch(Exception e)
-	      {
-	    	  e.printStackTrace();
-	      }
-	      finally {
-	    	  con.close();
-	    	  statement.close();
-	    	  resultSet.close();
-	      }
-		return categoryList;
+		ArrayList<Category> categoryList = new ArrayList<Category>();
+		String query = "SELECT category.idCategory, Name, Description FROM category INNER JOIN categoryinstore ON category.idCategory = categoryinstore.idCategory WHERE idStore = ?";
+
+		try (Connection con = DBConnection.createConnection();
+				Statement statement = con.createStatement();
+				PreparedStatement preparedStatement = con.prepareStatement(query)) {
+
+			preparedStatement.setInt(1, idStoreUser);
+
+			try (ResultSet resultSet = preparedStatement.executeQuery()) {
+
+				while (resultSet.next()) {
+					Category categoryBean = new Category();
+					User user = new User();
+					categoryBean.setIdCategory(resultSet.getInt("idCategory"));
+					categoryBean.setName(resultSet.getString("Name"));
+					categoryBean.setDescription(resultSet.getString("Description"));
+					categoryBean.setIdStore(idStoreUser);
+
+					categoryList.add(categoryBean);
+				}
+				return categoryList;
+			} catch (Exception e) {
+				log.log(Level.FINE, e.toString());
+			}
+
+		} catch (Exception e) {
+			log.log(Level.FINE, e.toString());
 		}
-	
-	
-	
+
+		return categoryList;
+
+	}
+
 	public int deleteCategory(int Idcategory) throws SQLException {
 		String query = "DELETE FROM category WHERE idCategory =" + Idcategory;
-		 Connection con = null;
-	     Statement statement = null;
-	     int result = 0;
-	     try {
-	    	  con = DBConnection.createConnection(); 
-	          statement = con.createStatement(); 
-	          result = statement.executeUpdate(query);
-	          return result;
-	     }		
-		 catch(Exception e){
-			e.printStackTrace();
+		int result = 0;
+		try (Connection con = DBConnection.createConnection(); Statement statement = con.createStatement()) {
+			result = statement.executeUpdate(query);
+			return result;
+		} catch (Exception e) {
+			log.log(Level.FINE, e.toString());
 		}
-	     finally {
-	    	 con.close();
-	    	 statement.close();
-	     }
-	     return result;
+
+		return result;
 	}
-	
-	public ArrayList<Category> getAllCategory(int idStoreUser) throws SQLException{
-		  ArrayList<Category> categoryList = new ArrayList<Category>();
-		  Connection con = null;
-	      Statement statement = null;
-	      ResultSet resultSet = null;
-	      try{        	 
 
-	          con = DBConnection.createConnection(); 
-	          statement = con.createStatement();
-	          String query = "SELECT category.idCategory, category.Name, category.Description FROM category WHERE category.idCategory NOT IN(SELECT idCategory FROM categoryinstore WHERE idStore = ?)";
-		        System.out.println(query);
+	public ArrayList<Category> getAllCategory(int idStoreUser) throws SQLException {
+		ArrayList<Category> categoryList = new ArrayList<Category>();
+		String query = "SELECT category.idCategory, category.Name, category.Description FROM category WHERE category.idCategory NOT IN(SELECT idCategory FROM categoryinstore WHERE idStore = ?)";
 
-	          	PreparedStatement preparedStatement  = con.prepareStatement(query);
-	      		preparedStatement.setInt(1, idStoreUser);
-	      		resultSet = preparedStatement.executeQuery(); 
-	          while(resultSet.next()) {
-	        	  Category categoryBean = new Category();
-	        	  categoryBean.setIdCategory(resultSet.getInt("idCategory"));
-	        	  categoryBean.setName(resultSet.getString("Name"));
-	        	  categoryBean.setDescription(resultSet.getString("Description"));
-	        	  categoryList.add(categoryBean);
-	          }
-	          return categoryList;
-	       }
-	      		
-	      catch(Exception e)
-	      {
-	    	  e.printStackTrace();
-	      }
-	      finally {
-	    	  con.close();
-	    	  statement.close();
-	    	  resultSet.close();
-	      }
+		try (Connection con = DBConnection.createConnection();
+				Statement statement = con.createStatement();
+				PreparedStatement preparedStatement = con.prepareStatement(query)) {
+
+			preparedStatement.setInt(1, idStoreUser);
+			try (ResultSet resultSet = preparedStatement.executeQuery()) {
+				while (resultSet.next()) {
+					Category categoryBean = new Category();
+					categoryBean.setIdCategory(resultSet.getInt("idCategory"));
+					categoryBean.setName(resultSet.getString("Name"));
+					categoryBean.setDescription(resultSet.getString("Description"));
+					categoryList.add(categoryBean);
+				}
+				return categoryList;
+			} catch (Exception e) {
+				log.log(Level.FINE, e.toString());
+			}
+		}
+
+		catch (Exception e) {
+			log.log(Level.FINE, e.toString());
+		}
 		return categoryList;
-		}
-	
-	
-	public int insertCategory(int idStore, int idCategory) throws SQLException {
-	        System.out.println(idCategory);
-	        String INSERT_USERS_SQL = "INSERT INTO categoryinstore" +
-	                "  (idStore, idCategory) VALUES " +
-	                " (?, ?);";
-		  Connection con = null;
-		  Statement statement = null;
-		  con = DBConnection.createConnection(); 
-
-		  PreparedStatement preparedStatement  = con.prepareStatement(INSERT_USERS_SQL);
-  		preparedStatement.setInt(1, idStore);
-  		preparedStatement.setInt(2, idCategory);
-		  int result = 0;
-		  try {
-		    	con = DBConnection.createConnection();
-		    	statement = con.createStatement();
-		    	result = preparedStatement.executeUpdate();
-		    	return result;
-		    }
-		    catch(Exception e)
-		    {
-		    	e.printStackTrace();
-		    }
-		  finally {
-			  con.close();
-			  statement.close();
-		  }
-		    return result;
-		  
 	}
-	
-	
-	
-	public int getIdCategoryByName(String name) throws SQLException{
-		  Connection con = null;
-	      Statement statement = null;
-	      ResultSet resultSet = null;
-	      int id = 0;
-	      try{ 
-	    	  String query_insert = "SELECT idCategory FROM category WHERE Name =  ?";
 
-
-	            con = DBConnection.createConnection(); 
-	        	PreparedStatement preparedStatement  = con.prepareStatement(query_insert);
-	        	preparedStatement.setString(1, name);
-
-	            resultSet = preparedStatement.executeQuery(); 
-
-	          while(resultSet.next()) {
-	        	   id = resultSet.getInt("idCategory");
-	          }
-	          return id;
-	       }
-	      		
-	      catch(Exception e)
-	      {
-	    	  e.printStackTrace();
-	      }
-	      finally {
-	    	  con.close();
-	    	  resultSet.close();
-	      }
-		return id;
+	public int insertCategory(int idStore, int idCategory) throws SQLException {
+		String insert_users_sql = "INSERT INTO categoryinstore" + "  (idStore, idCategory) VALUES " + " (?, ?);";
+		int result = 0;
+		try (Connection con = DBConnection.createConnection();
+				Statement statement = con.createStatement();
+				PreparedStatement preparedStatement = con.prepareStatement(insert_users_sql)) {
+			preparedStatement.setInt(1, idStore);
+			preparedStatement.setInt(2, idCategory);
+			result = preparedStatement.executeUpdate();
+			return result;
+		} catch (Exception e) {
+			log.log(Level.FINE, e.toString());
 		}
+		return result;
+	}
 
+	public int getIdCategoryByName(String name) throws SQLException {
+		int id = 0;
+		String query_insert = "SELECT idCategory FROM category WHERE Name =  ?";
+
+		try (Connection con = DBConnection.createConnection();
+				PreparedStatement preparedStatement = con.prepareStatement(query_insert)) {
+
+			preparedStatement.setString(1, name);
+			try (ResultSet resultSet = preparedStatement.executeQuery()) {
+				while (resultSet.next()) {
+					id = resultSet.getInt("idCategory");
+				}
+				return id;
+			} catch (Exception e) {
+				log.log(Level.FINE, e.toString());
+			}
+
+		} catch (Exception e) {
+			log.log(Level.FINE, e.toString());
+		}
+		return id;
+	}
 }
--- engSw2020.clup/src/main/webapp/DAO/StoreDao.java
+++ engSw2020.clup/src/main/webapp/DAO/StoreDao.java
@@ -5,89 +5,73 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-
-import main.webapp.model.Booking;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import main.webapp.model.Store;
-import main.webapp.model.User;
 import main.webapp.util.DBConnection;
 
 public class StoreDao {
-	
-	public Store getStoreInfo(int idStoreUser) throws SQLException{
-		  Store store = new Store();
-		  Connection con = null;
-	      Statement statement = null;
-	      ResultSet resultSet = null;
-	      
-	      try{        	 
-	    	  
+	private final static Logger log = Logger.getLogger(LoginDao.class.getName());
 
-	          con = DBConnection.createConnection(); 
-	          statement = con.createStatement();
-	          String query = "SELECT * FROM clup_engsw2020.store INNER JOIN User ON User.idStore = store.idStore WHERE User.idStore = ? ";
-	          
-	          	PreparedStatement preparedStatement  = con.prepareStatement(query);
-	      		preparedStatement.setInt(1, idStoreUser);
-	      		resultSet = preparedStatement.executeQuery(); 
-	          while(resultSet.next()) {
-	        	  store.setIdStore(resultSet.getInt("idStore"));
-	        	  store.setName(resultSet.getString("name"));
-	        	  store.setDesprition(resultSet.getString("description"));
-	        	  store.setTelephoneNumber(resultSet.getString("telephoneNumber"));
-	        	  store.setAddress(resultSet.getString("address"));
-	        	  store.setCity(resultSet.getString("city"));
-	        	  store.setCapacity(resultSet.getInt("capacity"));
-	        	  store.setBookableCapacity(resultSet.getInt("bookableCapacity"));
+	public Store getStoreInfo(int idStoreUser) throws SQLException {
+		Store store = new Store();
+		String query = "SELECT * FROM clup_engsw2020.store INNER JOIN User ON User.idStore = store.idStore WHERE User.idStore = ? ";
 
-	          }
-	          return store;
-	       }
-	      		
-	      catch(Exception e)
-	      {
-	    	  e.printStackTrace();
-	      }
-	      finally {
-	    	  con.close();
-	    	  statement.close();
-	    	  resultSet.close();
-	      }
+		try (Connection con = DBConnection.createConnection();
+				Statement statement = con.createStatement();
+				PreparedStatement preparedStatement = con.prepareStatement(query)) {
+			preparedStatement.setInt(1, idStoreUser);
+
+			try (ResultSet resultSet = preparedStatement.executeQuery()) {
+				while (resultSet.next()) {
+					store.setIdStore(resultSet.getInt("idStore"));
+					store.setName(resultSet.getString("name"));
+					store.setDesprition(resultSet.getString("description"));
+					store.setTelephoneNumber(resultSet.getString("telephoneNumber"));
+					store.setAddress(resultSet.getString("address"));
+					store.setCity(resultSet.getString("city"));
+					store.setCapacity(resultSet.getInt("capacity"));
+					store.setBookableCapacity(resultSet.getInt("bookableCapacity"));
+				}
+				return store;
+
+			} catch (Exception e) {
+				log.log(Level.FINE, e.toString());
+			}
+		} catch (Exception e) {
+			log.log(Level.FINE, e.toString());
+		}
+
 		return store;
-		}
-	public int updateData(Store store) throws SQLException{
-		  Connection con = null;
-	      Statement statement = null;
-	      ResultSet resultSet = null;
-	      int result = 0;
-	      try{        	 
-	          con = DBConnection.createConnection(); 
-	          statement = con.createStatement();
-	 	        System.out.println(store.getDesprition());
-	 	        System.out.println(store.getIdStore());
-	          String query = "update store set description = ?, telephoneNumber = ?, address = ?, city = ?, capacity = ?, bookableCapacity = ? where idStore = ?";
-	          PreparedStatement preparedStmt = con.prepareStatement(query);
-	          preparedStmt.setString(1, store.getDesprition());
-	          preparedStmt.setString(2, store.getTelephoneNumber());
-	          preparedStmt.setString(3, store.getAddress());
-	          preparedStmt.setString(4, store.getCity());
-	          preparedStmt.setInt(5, store.getCapacity());
-	          preparedStmt.setInt(6, store.getBookableCapacity());
-	          preparedStmt.setInt(7, store.getIdStore());
-	          result = preparedStmt.executeUpdate();	
 
-	          return result;
-	       }
-	      		
-	      catch(Exception e)
-	      {
-	    	  e.printStackTrace();
-	      }
-	      finally {
-	    	  con.close();
-	    	  statement.close();
-	      }
-		return result;
+	}
+
+	public int updateData(Store store) throws SQLException {
+
+		int result = 0;
+		String query = "update store set description = ?, telephoneNumber = ?, address = ?, city = ?, capacity = ?, bookableCapacity = ? where idStore = ?";
+
+		try (Connection con = DBConnection.createConnection();
+				Statement statement = con.createStatement();
+				PreparedStatement preparedStmt = con.prepareStatement(query)) {
+
+			preparedStmt.setString(1, store.getDesprition());
+			preparedStmt.setString(2, store.getTelephoneNumber());
+			preparedStmt.setString(3, store.getAddress());
+			preparedStmt.setString(4, store.getCity());
+			preparedStmt.setInt(5, store.getCapacity());
+			preparedStmt.setInt(6, store.getBookableCapacity());
+			preparedStmt.setInt(7, store.getIdStore());
+			result = preparedStmt.executeUpdate();
+
+			return result;
 		}
+
+		catch (Exception e) {
+			log.log(Level.FINE, e.toString());
+		}
+
+		return result;
+	}
 
 }
