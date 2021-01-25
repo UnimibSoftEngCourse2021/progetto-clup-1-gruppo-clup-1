78b9c533eb6c7203d8e55c981c4624173bc82cca
tommasozanelli <t.zanelli@campus.unimib.it>
2021-01-25 16:37:40 +0100

    Created new servlets

---
+++ engSw2020.clup/src/main/webapp/web/GetMaxCategoryServlet.java
@@ -0,0 +1,68 @@
+package main.webapp.web;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.google.gson.Gson;
+
+import main.webapp.DAO.CategoryDao;
+
+/**
+ * Servlet implementation class GetMaxCategoryServlet
+ */
+public class GetMaxCategoryServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	Logger log = Logger.getLogger(GetMaxCategoryServlet.class.getName());
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GetMaxCategoryServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		CategoryDao categoryDao = new CategoryDao();
+		String name ="";
+		HttpSession session = request.getSession();
+		int idStore = (Integer)session.getAttribute("id");
+		int idCategory=0;
+		try 
+		{
+			idCategory=categoryDao.getMaxIdCategory(idStore);
+			if(idCategory > 0) 
+			{
+				name = categoryDao.getNameFromId(idCategory);
+			}
+		}
+		catch(Exception e) 
+		{
+			log.log(Level.FINE, e.toString());
+		}
+		String json = new Gson().toJson(name);
+
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		response.getWriter().write(json);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
