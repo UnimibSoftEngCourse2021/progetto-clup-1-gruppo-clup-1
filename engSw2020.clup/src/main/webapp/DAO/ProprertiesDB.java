ba40e228537badd9ed408c9c5521e10ab170c89f
Nasic-Elvis <e.nasic@campus.unimib.it>
2021-01-27 10:15:17 +0100

    Added condif.properties file

---
+++ engSw2020.clup/src/main/webapp/DAO/ProprertiesDB.java
@@ -0,0 +1,25 @@
+package main.webapp.DAO;
+
+public class ProprertiesDB {
+	private String dbUrl;
+	private String dbPassword;
+	private String dbUsername;
+	public String getDbUrl() {
+		return dbUrl;
+	}
+	public void setDbUrl(String dbUrl) {
+		this.dbUrl = dbUrl;
+	}
+	public String getDbPassword() {
+		return dbPassword;
+	}
+	public void setDbPassword(String dbPassword) {
+		this.dbPassword = dbPassword;
+	}
+	public String getDbUsername() {
+		return dbUsername;
+	}
+	public void setDbUsername(String dbUsername) {
+		this.dbUsername = dbUsername;
+	}
+}
